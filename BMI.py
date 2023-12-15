bfbd74580ddc78a0867cce32d282b8235b7aa49e
Swastisudha Dash <swastisudhadash7@gmail.com>
2023-12-16 01:07:16 +0530

    Update BMI.py

--- BMI.py
+++ BMI.py
@@ -1,86 +1,36 @@
-import pyttsx3 
-import speech_recognition as sr 
-import datetime
-import wikipedia 
-import webbrowser
-import os
-import smtplib
+import tkinter as tk
+from tkinter import ttk
 
-engine = pyttsx3.init('sapi5')
-voices = engine.getProperty('voices')
-engine.setProperty('voice', voices[1].id)
-
-
-def speak(audio):
-    engine.say(audio)
-    engine.runAndWait()
-
-
-def wish():
-    hour = int(datetime.datetime.now().hour)
-    if hour>=0 and hour<12:
-        speak("Good Morning Sir!")
-
-    elif hour>=12 and hour<18:
-        speak("Good Afternoon Sir!")   
-
-    else:
-        speak("Good Evening sir!")  
-
-    speak("I am Rio. How may I help you sir!")       
-
-def tcmd():
-    r = sr.Recognizer()
-    with sr.Microphone() as source:
-        print("Listening...")
-        r.pause_threshold = 1
-        audio = r.listen(source)
-
+def bmi_cal():
     try:
-        print("Recognizing...")    
-        query = r.recognize_google(audio, language='en-in')
-        print(f"User said: {query}\n")
+        weight = float(weight_val.get())
+        height = float(height_val.get())
+        bmi = round(weight / (height ** 2), 2)
+        result_lal.config(text=f'Your BMI value: {bmi}')     
+    except ValueError:
+        result_lal.config(text='Use valid numbers for weight and height.')
 
-    except Exception as e:
-        print("Say that again please...")  
-        return "None"
-    return query
+window = tk.Tk()
+window.title('Python BMI Calculator')
 
+style = ttk.Style()
+style.theme_use('default')
 
-if __name__ == "__main__":
-    wish()
-    while True:
-        query = tcmd().lower()
-        if 'wikipedia' in query:
-            speak('Searching Wikipedia...')
-            query = query.replace("wikipedia", "")
-            results = wikipedia.summary(query, sentences=1)
-            speak("According to Wikipedia")
-            print(results)
-            speak(results)
+frame = ttk.Frame(window, padding='15')
+frame.grid(row=0, column=0, padx=15, pady=15)
 
-        elif 'open youtube' in query:
-            webbrowser.open("youtube.com")
+ttk.Label(frame, text='Weight(kg):').grid(row=0, column=0, padx=12, pady=12)
+weight_val = ttk.Entry(frame)
+weight_val.grid(row=0, column=1, padx=12, pady=12)
 
-        elif 'open google' in query:
-            webbrowser.open("google.com")
+ttk.Label(frame, text='Height(M):').grid(row=1, column=0, padx=12, pady=12)
+height_val = ttk.Entry(frame)
+height_val.grid(row=1, column=1, padx=12, pady=12)
 
-        elif 'open stack' in query:
-            webbrowser.open("stackoverflow.com")
-        
-        elif 'open gmail' in query:
-            webbrowser.open("gmail.com")
-            
-        elif 'play music' in query:
-            music_dir = 'D:\\music'
-            songs = os.listdir(music_dir)
-            print(songs)    
-            os.startfile(os.path.join(music_dir, songs[0]))
+calculate_btn = ttk.Button(frame, text='BMI Calculate', command=bmi_cal)
+calculate_btn.grid(row=2, column=0, columnspan=2, pady=12)
 
-        elif 'time' in query:
-            strTime = datetime.datetime.now().strftime("%H:%M:%S")    
-            speak(f"Sir, the time is {strTime}")
+result_lal = ttk.Label(frame, text='')
+result_lal.grid(row=3, column=0, columnspan=2, pady=12)
 
-        elif 'open code' in query:
-            codePath = "C:\\Users\\Haris\\AppData\\Local\\Programs\\Microsoft VS Code\\Code.exe"
-            os.startfile(codePath) 
+window.mainloop()
